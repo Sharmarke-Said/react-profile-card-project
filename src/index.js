9d6cdc2605a440755b693de21385da520c6e3f76
Sharmarke Said <sharmakesaid17@gmail.com>
2024-04-27 21:05:05 +0300

    Applied some react concepts for shortering e.g props distructuring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML&CSS",
+    level: "Intermediate",
+    color: "#FFA500", // orange
+  },
+  {
+    skill: "JavaScript",
+    level: "Advanced",
+    color: "#FFFF00", // yellow
+  },
+  {
+    skill: "React",
+    level: "Intermediate",
+    color: "#0000FF", // blue
+  },
+  {
+    skill: "Python",
+    level: "Beginner",
+    color: "#008000", // green
+  },
+  {
+    skill: "Java",
+    level: "Advanced",
+    color: "#800080", // purple
+  },
+  {
+    skill: "MySQL",
+    level: "Intermediate",
+    color: "#00FFFF", // cyan
+  },
+];
 
 function App() {
   return (
@@ -36,23 +70,40 @@ function Intro() {
 }
 
 function SkillList() {
+  // return (
+  //   <div className="skill-list">
+  //     <Skill skill="HTML&CSS" emoji="👍" color="orange" />
+  //     <Skill skill="JavaScript" emoji="👌" color="yellow" />
+  //     <Skill skill="React" emoji="👍" color="blue" />
+  //     <Skill skill="Python" emoji="💪" color="green" />
+  //     <Skill skill="Java" emoji="👍" color="purple" />
+  //     <Skill skill="MySQL" emoji="👌" color="cyan" />
+  //   </div>
+  // );
   return (
     <div className="skill-list">
-      <Skill skill="HTML&CSS" emoji="👍" color="orange" />
-      <Skill skill="JavaScript" emoji="👌" color="yellow" />
-      <Skill skill="C" emoji="👍" color="blue" />
-      <Skill skill="Python" emoji="💪" color="green" />
-      <Skill skill="Java" emoji="👍" color="purple" />
-      <Skill skill="MySQL" emoji="👌" color="cyan" />
+      {/* <ul> */}
+      {skills.map((skill) => (
+        <Skill skillObj={skill} key={skill.skill} />
+      ))}
+      {/* </ul> */}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skillObj }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill} </span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: skillObj.color }}>
+      <span>{skillObj.skill} </span>
+      <span>
+        {skillObj.level === "Advanced"
+          ? "💪"
+          : skillObj.level === "Intermediate"
+          ? "👌"
+          : skillObj.level === "Beginner"
+          ? "👍"
+          : ""}
+      </span>
     </div>
   );
 }
